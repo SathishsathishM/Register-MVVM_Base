ff8b29d7f924cec200c18f7ebee357ab8ab8695c
SathishsathishM <141834101+SathishsathishM@users.noreply.github.com>
2024-01-31 15:08:49 +0530

    MVVMUsed

---
+++ MVVMTask/ViewModel/Eventcommad.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVMTask.ViewModel
+{
+    public class Eventcommad : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        private Action _action;
+        public Eventcommad(Action action)
+        {
+            _action = action;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+           return true;
+        }
+
+    public void Execute(object? parameter)
+       {
+          _action.Invoke();
+       }
+       
+    }
+}
---
+++ MVVMTask/ViewModel/HomeViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Data.SqlClient;
+using MVVMTask.Model;
+using System.Windows;
+
+namespace MVVMTask.ViewModel
+{
+    public class HomeViewModel
+    {
+        public Student StudentValue { get; set; }
+
+        public Eventcommad Createaccount {  get; set; }
+
+        public HomeViewModel() 
+        {
+            Createaccount = new Eventcommad(Register);
+            if (StudentValue == null )
+            {
+                StudentValue = new Student();
+            }
+            StudentValue.Name = "sathish";
+            StudentValue.Age = 18;
+            StudentValue.Location = "Vellore";
+
+        }
+        public void Register()
+        {
+            string name = StudentValue.Name;
+            int age= StudentValue.Age;
+            string location = StudentValue.Location;
+            //sqlconnection
+            SqlConnection sql = new SqlConnection();
+            sql.ConnectionString = "Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=OCT05;Data Source=DESKTOP-2MPHQ9B\\SQLEXPRESS;Encrypt=false;";
+            sql.Open();
+            //string create = $"Create Table MVVM(Name Nvarchar(20),Age Numeric(20),Location Nvarchar(20))";
+            string insert = $"Insert into MVVM Values('{name}','{age}','{location}')";
+            SqlCommand sqlCommand=new SqlCommand(insert, sql);
+            sqlCommand.Connection = sql;
+            object val= sqlCommand.ExecuteNonQuery();
+            sql.Close();
+
+            MessageBox.Show("Reg Success");
+        }
+    }
+}
